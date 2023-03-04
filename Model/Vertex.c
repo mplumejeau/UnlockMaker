f88cca12693ae52be9554b5183e7bb5e04a56077
Maxime <maxime@Ubuntu22.04.myguest.virtualbox.org>
2023-03-04 17:47:23 +0100

    ajout vertex.c avec documentation fonctions + mise à jour du CMakeList pour integrer ce fichier

---
+++ Model/Vertex.c
@@ -0,0 +1,41 @@
+//
+// Created by maxime on 04/03/23.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Vertex.h"
+
+/**
+ * allocate a Vertex in the dynamic space memory
+ * @return Vertex* the address of the allocated structure
+ */
+Vertex* allocVertex(){
+    return NULL;
+}
+
+/**
+ * initialize data for a vertex
+ * @param v the address of the vertex to be initialized
+ * @param c the address of the card pointed by the vertex
+ */
+void initVertex(Vertex* v, Card* c){
+
+}
+
+/**
+ * return the address of the card pointed by the vertex
+ * @param v the address of the vertex
+ * @return Card* the address of the card
+ */
+Card* getVertexCard(Vertex* v){
+    return NULL;
+}
+
+/**
+ * free memory stored in a vertex
+ * @param v the vertex to be freed
+ */
+void freeVertex(Vertex* v){
+    
+}
