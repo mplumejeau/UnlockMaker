23f9e94794da09dde4d6ba0713531fe53af22a84
Thomas <vboxuser@VM-UBUNTU.myguest.virtualbox.org>
2023-05-01 19:33:48 +0200

    Add devControl branch

---
+++ Controller/Controller.c
@@ -0,0 +1,5 @@
+/*
+// Created by vboxuser on 01/05/23.
+*/
+
+#include "Controler.h"
---
+++ Controller/Controller.h
@@ -0,0 +1,8 @@
+/*
+// Created by vboxuser on 01/05/23.
+*/
+
+#ifndef UNLOCKMAKER_CONTROLER_H
+#define UNLOCKMAKER_CONTROLER_H
+
+#endif /*UNLOCKMAKER_CONTROLER_H*/
