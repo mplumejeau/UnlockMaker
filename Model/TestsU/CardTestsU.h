11a5bb9e71f0ec129ffb3dbaf220d2dd0e103537
yminiya <Yasser.Miniya@insa-rennes.com>
2023-06-01 20:03:24 +0200

    TESTU for Card.c

--- Model/TestsU/CardTestsU.h
+++ Model/TestsU/CardTestsU.h
@@ -6,3 +6,24 @@
 #define UNLOCKMAKER_CARDTESTSU_H
 
 #endif //UNLOCKMAKER_CARDTESTSU_H
+
+
+#ifndef CARDTESTSU_H
+#define CARDTESTSU_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+// Fonctions de configuration et de nettoyage des tests
+int setupAllocCard(void** state);
+int teardownAllocCard(void** state);
+
+// Déclarations des fonctions de test
+void testSetCardType(void** state);
+void testSetCardNumber(void** state);
+void testFixCardNumber(void** state);
+
+#endif // CARDTESTSU_H
+
